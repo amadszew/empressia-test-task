7181dd5fbec86838917a5f135ac48bd32209d830
Amadeusz <amadeusz0szewczyk@gmail.com>
2020-12-10 23:21:32 +0100

    media breakpoints fixed, px to rem function added, intro section - mobile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import Header from './components/Header/Header';
+import Intro from './components/Intro/Intro';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header />
+        <main>
+          <Intro />
+        </main>
       </div>
     </div>
   );
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import skypescreen from '../../assets/images/illustration-hero.png';
+
+const Intro = () => {
+  return (
+    <section className="intro">
+      <div className="intro__consultation">
+        <header>
+          <h2 className="heading">Konsultacje z lekarzem on-line</h2>
+        </header>
+
+        <p className="intro__consultation__text">Jeżeli potrzebujesz pomocy związanej ze skórą, włosami, paznokciami lub natury intymnej, jesteś we właściwym miejscu. Dzięki naszej pomocy zadbasz o zdrowie i piękno swego ciała.</p>
+
+        <div className="intro__consultation__buttons">
+          <button className="btn btn__primary btn__primary--icon">
+            <div className="icon__text">Zarezerwuj termin</div>
+            <div className="icon__box">
+              <span></span>
+            </div>
+          </button>
+          <button className="btn btn__ghost">Jak to działa?</button>
+        </div>
+      </div>
+      <div className="intro__img">
+        <img src={skypescreen} alt="skype screen with video call" />
+      </div>
+    </section>
+  );
+}
+
+export default Intro;
